3841dd8bc219ffa650f1ab8e15d94610189d9306
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-02-20 00:07:18 +0530

    throttling added

--- code/Javascript/HIgherConcepts/throttling.js
+++ code/Javascript/HIgherConcepts/throttling.js
@@ -1,1 +1,19 @@
 // rate at whoch the function is invoked
+ function throttle(func, delay){
+    let lastCall=0;
+    return function(...args){
+
+        const now= new Data().getTime();
+        if(now -lastCall >= delay){
+            func(...args);
+            lastCall=now;
+        }
+
+    }
+ }
+
+ const throttledScrollHandler=throttle(()=>{
+    console.log("loading more content..")
+ }, 100)
+
+ window.addEventListener("Scroll", throttledScrollHandler)
