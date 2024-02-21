a000e189ef7e7e5a1ddc7af5b4975077fea29964
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-02-22 00:59:38 +0530

    this-keyword-strict-nonstrict

---
+++ code/Javascript/thisKeyword.js
@@ -0,0 +1,40 @@
+"use strict"
+
+// this keyword works deferblty in differnet scenarios
+console.log(this) 
+// window object 
+// global object 
+// js can run indsise anywhere , and there is a js runtime env , 
+// inside the brower the global obhjevt is different every where 
+// in vcase of   brower the valuye of global object is global 
+
+// depends upon where we are running that code 
+
+
+
+
+
+// this in global scope
+this
+function x(){
+    console.log(this)
+    // again a window , but this is different
+    // it is a non strict mode
+    // this keyw the value depends on strict , non strict mode 
+    // window-> non strict
+    // undefined -> strict mode 
+}
+x()
+
+// this inside a function - this substituion
+
+// if the value is undeifned or null 
+// this will be replaced with global object 
+// only in on strict mode 
+
+
+// this keyoerd also depends how the function is called
+x();  undefined // called without any reference in strict mode 
+window.x(); // returns window object  // called with refrtence even with strict mode on
+
+// this in strict node (this substituion)
