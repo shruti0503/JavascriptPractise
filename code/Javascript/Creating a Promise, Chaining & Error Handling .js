90154182e8379f60fae4a98e7f5d5d3fc7d15fb5
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-02-26 21:59:40 +0530

    upda

--- code/Javascript/Creating a Promise, Chaining & Error Handling .js
+++ code/Javascript/Creating a Promise, Chaining & Error Handling .js
@@ -2,10 +2,10 @@
 const cart=["shoes", "kurta"];
 // creating order with an api -> returns orderis => proceed to payemnt
 const promise=createOrder(cart);
-promise.then(function(){
-    proceedToPayment(orderId);
+// promise.then(function(){
+//     proceedToPayment(orderId);
 
-)}
+// )}
 // error handling
 
 //promise chianing
