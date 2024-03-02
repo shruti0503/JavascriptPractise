a7eb5706970378f284d3a6092b82e07c394889fe
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-02 14:57:15 +0530

    promiseChaining

--- code/Javascript/Creating a Promise, Chaining & Error Handling .js
+++ code/Javascript/Creating a Promise, Chaining & Error Handling .js
@@ -1,11 +1,11 @@
 // creating our own promise 
 const cart=["shoes", "kurta"];
 // creating order with an api -> returns orderis => proceed to payemnt
-const promise=createOrder(cart);
-// promise.then(function(){
-//     proceedToPayment(orderId);
+const promise=createOrder(cart); // return order id after creatung order
+// expectation-> going to return a promise
 
-// )}
-// error handling
+promise.then(function(orderId){
+    proceedToPayment(orderId);
 
-//promise chianing
+})
+// ----> now lets see how order id is created 
