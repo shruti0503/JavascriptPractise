77e920fb5a603b25ea2d71522bde04d3f0b7ea47
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-11 22:17:04 +0530

    Update 1.js

--- code/Javascript/PromiseAPIs/1.js
+++ code/Javascript/PromiseAPIs/1.js
@@ -142,7 +142,7 @@ const P1 = new Promise((resolve, reject) => {
 // Promise.all(promises) – waits for all promises to resolve and returns an array of their results. If any of the given promises rejects, it becomes the error of Promise.all, and all other results are ignored.
 
 // Promise.allSettled(promises) (recently added method) – waits for all promises to settle and returns their results as an array of objects with: status: “fulfilled” or “rejected” value (if fulfilled) or reason (if rejected).
-
+///
 // Promise.race(promises) – waits for the first promise to settle, and its result/error becomes the outcome.
 
 // Promise.any(promises) (recently added method) – waits for the first promise to fulfill, and its result becomes the outcome. If all of the given promises are rejected, AggregateError becomes the error of Promise.any.
