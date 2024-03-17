8c7d18893b8cb1046ef9920b601aa98989c79c35
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-03-17 23:26:12 +0530

    Update 3.js

--- code/Javascript/asynAwait/3.js
+++ code/Javascript/asynAwait/3.js
@@ -81,7 +81,10 @@ handlePromise().catch(err => console.log(err)); // this will work as handlePromi
 //async-await is just a syntactic sugar around promise. 
 //Behind the scene async-await is just promise. So both are same, 
 //it’s just async-await is new way of writing code. 
+// NWER WAY 
 //async-await solves few of the short-coming of Promise like Promise Chaining.
 // async-await also increases the readability. So sort of it is always advisable to use async-await.
 
+
+// PUT DOWN MORE SUCH EXMAPLE CASES
 
